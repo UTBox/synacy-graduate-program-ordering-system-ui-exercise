673b00d134160e5828290a3acb30f4479cd7df0c
Carlina-Amaba <carlina.amaba@synacy.com>
2023-08-15 12:51:10 +0800

    Added product pages

---
+++ src/app/product/service/product.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@angular/core";
+import {ProductRepository} from "../repository/product.repository";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ProductService {
+  constructor(private productRepository: ProductRepository) {
+  }
+
+  public fetchProducts(){
+    return this.productRepository.getProducts();
+  }
+
+}
