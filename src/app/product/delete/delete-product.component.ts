97354621e4bd8c4b71c6a44a80c7004d0fd22fb2
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-17 07:25:44 +0800

    Added delete component

---
+++ src/app/product/delete/delete-product.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {AlertState} from "../../shared/alert/state.enum";
+import {ProductService} from "../service/product.service";
+import {RouterService} from "../service/router.service";
+
+@Component({
+  selector: 'app-delete',
+  templateUrl: './delete-product.component.html',
+  styleUrls: ['./delete-product.component.css']
+})
+export class DeleteProductComponent {
+
+  protected readonly alertState = AlertState
+  public errorMessage: any = ''
+  public successMessage: any = ''
+
+  constructor(private productService: ProductService, private routerService: RouterService) {
+  }
+
+  onDelete() {
+    this.productService.removeProduct(this.routerService.getQueryParams().product.id).subscribe(
+      () => {
+        this.successMessage = 'Product removed successfully.';
+        // Perform any additional actions after successful deletion
+      },
+      (error) => {
+        this.errorMessage = 'Failed to remove product. Please try again.';
+        console.error(error);
+      }
+    );
+  }
+}
