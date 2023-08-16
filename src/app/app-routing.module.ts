3a8ded205abc1e258953df6d48abc4af528292a2
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-17 07:26:13 +0800

    Added routing service for edit and delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ProductComponent} from "./product/product.component";
 import {AddProductComponent} from "./product/add/add-product.component";
-import {EditComponent} from "./product/edit/edit.component";
+import {EditProductComponent} from "./product/edit/edit-product.component";
+import {DeleteProductComponent} from "./product/delete/delete-product.component";
 
 const routes: Routes = [
   {
@@ -10,7 +11,8 @@ const routes: Routes = [
     children: [
       {path: '', component: ProductComponent},
       {path: 'add', component: AddProductComponent},
-      {path: 'edit', component: EditComponent}
+      {path: 'edit', component: EditProductComponent},
+      {path: 'delete', component: DeleteProductComponent}
     ]
   },
   {
