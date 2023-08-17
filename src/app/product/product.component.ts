1437f7359e9995c3597c1ddf8da2ced8d0434f8a
junsaku0 <junsaku.yamada@synacy.com>
2023-08-17 15:56:17 +0800

    Add pagination and navigation buttons

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ProductService} from "./service/product.service"
 import {PageResponse} from "./model/page-response.model";
-import {IProduct} from "./model/product.model";
 import {RouterService} from "./service/router.service";
 
 @Component({
@@ -16,7 +15,17 @@ export class ProductComponent implements OnInit {
     pageNumber: 0,
     totalCount: 0
   };
-  public readonly MAX_LIMIT = 20;
+
+  @Input() isShowPrev = false;
+  @Input() isShowNext = true;
+  @Input() state = 'page1';
+
+  public pages: any;
+
+  public readonly MAX_LIMIT = 5;
+  public pageNumber = 1;
+  public totalPages = 0;
+
   public message: string = '';
   public deleteMessage: boolean = false;
   constructor(private productService: ProductService, private routerService: RouterService) {
@@ -30,11 +39,34 @@ export class ProductComponent implements OnInit {
   }
 
   private initializeProducts(): void {
-    this.productService.fetchProducts(this.MAX_LIMIT, 1)
-      .subscribe({ next: (data: PageResponse) =>  {
-        console.log('Response:', data);
-        this.productsInPage = data;
-        }})
+    this.productService.fetchProducts(this.MAX_LIMIT, this.pageNumber)
+      .subscribe({
+        next: (data: PageResponse) =>
+        {
+          console.log('Response:', data);
+          this.productsInPage = data;
+          this.createPageNav()
+          if (this.totalPages < 2) {
+            this.isShowNext = false;
+          }
+        }
+      })
+  }
+
+  private fetchPagedProducts(): void {
+    this.productService.fetchProducts(this.MAX_LIMIT, this.pageNumber)
+      .subscribe({
+        next: (data: PageResponse) =>
+        {
+          console.log('Response:', data);
+          this.productsInPage = data;
+        }
+      })
+  }
+
+  private createPageNav() {
+    this.totalPages = Math.ceil(this.productsInPage.totalCount/this.productsInPage.content.length);
+    this.pages = Array.from(Array(this.totalPages).keys()).map(x => x+1);
   }
 
   public editProduct(product: any) {
@@ -46,5 +78,30 @@ export class ProductComponent implements OnInit {
     console.log('delete product', product);
     this.deleteMessage = true;
     this.routerService.navigate('/product/delete', {'product': product});
+  }
+
+  public prevPage() {
+    this.pageNumber -= 1;
+    this.initializeProducts();
+    this.isShowNext = true;
+    if (this.pageNumber == 1) {
+      this.isShowPrev = false;
+    }
+  }
+
+  public nextPage() {
+    this.pageNumber += 1;
+    this.fetchPagedProducts();
+    this.isShowPrev = true;
+    if (this.pageNumber == this.totalPages) {
+      this.isShowNext = false;
+    }
+  }
+
+  public navPage(page: number) {
+    this.pageNumber = page;
+    this.fetchPagedProducts();
+    this.isShowPrev = this.pageNumber != 1;
+    this.isShowNext = this.pageNumber != this.totalPages;
   }
 }
